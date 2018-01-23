181efb9f39a660c5cdc428d836f0d7f985a3212d
Ihar Yakimush <iharyakimush@gmail.com>
2018-01-23 14:26:50 +0300

    add tests

--- Community.Data.OData.Linq.xTests/ODataLinqExtensionsTests.cs
+++ Community.Data.OData.Linq.xTests/ODataLinqExtensionsTests.cs
@@ -7,12 +7,21 @@
     public class ODataLinqExtensionsTests
     {
         [Fact]
-        public void WhereFirstTime()
+        public void WhereSimple1()
         {
             var result = TestClass.CreateQuery().OData().Where("Id eq 1").ToArray();
             
             Assert.Single(result);
             Assert.Equal(1, result[0].Id);
+        }
+
+        [Fact]
+        public void WhereSimple2()
+        {
+            var result = TestClass.CreateQuery().OData().Where("Name eq 'n1'").ToArray();
+
+            Assert.Single(result);
+            Assert.Equal("n1", result[0].Name);            
         }
     }
 }
