d2373f73e2a53cfba9c6e1fd2f1597037b83d6dd
Ihar Yakimush <iharyakimush@gmail.com>
2018-01-26 23:48:40 +0300

    add build.ps1 and resign

---
+++ build.ps1
@@ -0,0 +1,7 @@
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+dotnet restore .\Community.Data.OData.Linq\Community.OData.Linq.csproj
+
+dotnet build .\Community.Data.OData.Linq\Community.OData.Linq.csproj -c Release
+
+# dotnet pack .\Community.Data.OData.Linq\Community.OData.Linq.csproj -c Release -o .\artifacts
