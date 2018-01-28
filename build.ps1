09f5ddd22d7e1c686262977519bfccebf7a3ee9c
Ihar Yakimush <iharyakimush@gmail.com>
2018-01-28 12:49:23 +0300

    update build file

--- build.ps1
+++ build.ps1
@@ -1,7 +1,2 @@
-if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
-
-dotnet restore .\Community.Data.OData.Linq\Community.OData.Linq.csproj
-
-dotnet build .\Community.Data.OData.Linq\Community.OData.Linq.csproj -c Release
-
-# dotnet pack .\Community.Data.OData.Linq\Community.OData.Linq.csproj -c Release -o ..\artifacts
+dotnet restore .\Community.Data.OData.Linq.sln
+dotnet build .\Community.Data.OData.Linq.sln -c Release
