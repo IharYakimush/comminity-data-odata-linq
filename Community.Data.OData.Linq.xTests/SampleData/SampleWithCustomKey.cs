ddb4cb8d0170cf7ad17d2511e8b937408045ceab
Ihar Yakimush <iharyakimush@gmail.com>
2018-02-01 23:25:21 +0300

    InvalidOperationException: The entity does not have a key defined
    https://github.com/IharYakimush/comminity-data-odata-linq/issues/5

---
+++ Community.Data.OData.Linq.xTests/SampleData/SampleWithCustomKey.cs
@@ -0,0 +1,25 @@
+﻿namespace Community.OData.Linq.xTests.SampleData
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    public class SampleWithCustomKey
+    {
+        private static readonly SampleWithCustomKey[] items =
+            {
+                new SampleWithCustomKey { Name = "n1", DateTime = new DateTime(2018, 1, 26)},
+                new SampleWithCustomKey { Name = "n2", DateTime = new DateTime(2001, 1, 26)}
+            };
+
+        public static IQueryable<SampleWithCustomKey> CreateQuery()
+        {
+            return items.AsQueryable();
+        }
+
+        [Key]
+        public string Name { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
