1d80fb43b316143adf34bb547419389e988fac18
Ihar Yakimush <IharYakimush@gmail.com>
2018-07-13 13:47:39 +0300

    add unit tests for string functions

--- Community.Data.OData.Linq.xTests/FilterFunctionsTests.cs
+++ Community.Data.OData.Linq.xTests/FilterFunctionsTests.cs
@@ -9,12 +9,21 @@ namespace Community.OData.Linq.xTests
     public class FilterFunctionsTests
     {
         [Fact]
-        public void Substring()
+        public void Contains()
         {
             var result = SimpleClass.CreateQuery().OData().Filter("contains(Name,'1')").ToArray();
 
             Assert.Single(result);
             Assert.Equal("n1", result[0].Name);
+        }
+
+        [Fact]
+        public void Substring()
+        {
+            var result = SimpleClass.CreateQuery().OData().Filter("substring(Name, 1) eq '2'").ToArray();
+
+            Assert.Single(result);
+            Assert.Equal("n2", result[0].Name);
         }
     }
 }
