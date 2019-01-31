e09ef0b6b009f074e8f73d1a81c6deef99d281be
Ihar Yakimush <IharYakimush@gmail.com>
2019-01-31 14:42:49 +0300

    https://github.com/IharYakimush/comminity-data-odata-linq/issues/31

---
+++ Community.Data.OData.Linq.xTests/Issues/Issue31.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Community.OData.Linq.xTests.Issues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+
+    using Community.OData.Linq.xTests.SampleData;
+
+    using Microsoft.OData;
+
+    using Xunit;
+
+    public class Issue31
+    {
+        [Fact]
+        public void WhereWithInThrowException()
+        {
+            Exception exc = Assert.Throws<ODataException>(() => SimpleClass.CreateQuery().OData().Filter("Id in (1,100)").ToArray());
+            Assert.Contains("Syntax error", exc.Message);
+        }
+    }
+}
