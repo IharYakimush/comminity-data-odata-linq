415f096d743dfe266fb23c1dbcb896fb54d3418c
IharYakimushEpam <Ihar_Yakimush@epam.com>
2022-06-05 19:07:28 +0300

    test nuget

--- pack.ps1
+++ pack.ps1
@@ -1,4 +1,3 @@
-dotnet build -c Release
-dotnet pack .\Community.Data.OData.Linq\Community.OData.Linq.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -o .\nupkg
-dotnet pack .\Community.OData.Linq.Json\Community.OData.Linq.Json.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -o .\nupkg
-dotnet pack .\Community.OData.Linq.AspNetCore\Community.OData.Linq.AspNetCore.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -o .\nupkg
+dotnet pack .\Community.Data.OData.Linq\Community.OData.Linq.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:VersionPrefix=0-local -o .\nupkg
+dotnet pack .\Community.OData.Linq.Json\Community.OData.Linq.Json.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:VersionPrefix=0-local -o .\nupkg
+dotnet pack .\Community.OData.Linq.AspNetCore\Community.OData.Linq.AspNetCore.csproj -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:VersionPrefix=0-local -o .\nupkg
